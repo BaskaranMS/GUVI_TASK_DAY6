025528c6d4c7be4b8beba2256f3ecdaa263a9604
BaskaranMS <official.ms.baskaran@gmail.com>
2023-12-06 14:15:39 +0530

    task-6

---
+++ person.js
@@ -0,0 +1,61 @@
+//person class
+class Person {
+    constructor(firstName, lastName, age, gender, address, phoneNumber, email) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.gender = gender;
+      this.address = address;
+      this.phoneNumber = phoneNumber;
+      this.email = email;
+      this.hobbies = [];
+      this.friends = [];
+    }
+  
+    // Method to add a hobby
+    addHobby(hobby) {
+      this.hobbies.push(hobby);
+    }
+  
+    // Method to add a friend
+    addFriend(friend) {
+      this.friends.push(friend);
+    }
+  
+    // Method to get full name
+    getFullName() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  
+    // Method to display details
+    displayDetails() {
+      console.log(`Name: ${this.getFullName()}`);
+      console.log(`Age: ${this.age}`);
+      console.log(`Gender: ${this.gender}`);
+      console.log(`Address: ${this.address}`);
+      console.log(`Phone Number: ${this.phoneNumber}`);
+      console.log(`Email: ${this.email}`);
+      console.log(`Hobbies: ${this.hobbies.join(', ')}`);
+      console.log(`Friends: ${this.friends.map(friend => friend.getFullName()).join(', ')}`);
+    }
+  }
+
+  // Create persons
+  const person1 = new Person("John", "Doe", 30, "Male", "123 Main St", "555-1234", "john.doe@example.com");
+  const person2 = new Person("Jane", "Smith", 25, "Female", "456 Oak St", "555-5678", "jane.smith@example.com");
+  
+  // Add hobbies
+  person1.addHobby("Reading");
+  person1.addHobby("Traveling");
+  person2.addHobby("Painting");
+  person2.addHobby("Hiking");
+  
+  // Add friends
+  person1.addFriend(person2);
+  person2.addFriend(person1);
+  
+  // Display details
+  person1.displayDetails();
+  console.log('\n----------------\n');
+  person2.displayDetails();
+  
---
+++ uberPriceCalculator.js
@@ -0,0 +1,28 @@
+class UberPriceCalculator {
+    constructor(baseFare, costPerMinute, costPerMile) {
+      this.baseFare = baseFare;
+      this.costPerMinute = costPerMinute;
+      this.costPerMile = costPerMile;
+    }
+  
+    calculatePrice(distance, time) {
+      const distanceCost = distance * this.costPerMile;
+      const timeCost = time * this.costPerMinute;
+      const totalPrice = this.baseFare + distanceCost + timeCost;
+  
+      return totalPrice;
+    }
+  }
+  
+  // Example usage:
+  
+  // Create an Uber price calculator with rates
+  const uberCalculator = new UberPriceCalculator(5, 0.2, 1.5);
+  
+  // Calculate the price for a ride
+  const distanceTraveled = 10; // in miles
+  const timeSpent = 15; // in minutes
+  const totalPrice = uberCalculator.calculatePrice(distanceTraveled, timeSpent);
+  
+  console.log(`Total Uber price: $${totalPrice.toFixed(2)}`);
+  
